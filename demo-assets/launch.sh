ec8b2b78d97bb63278143c367e7da0a90ec42ee0
Ash Wilson <ash.d.wilson@gmail.com>
2017-10-15 18:21:21 -0700

    chg: dev: Demo assets added !wip

---
+++ demo-assets/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+java -Xmx128m \
+     -Dlogback.configurationFile=./logback.xml \
+     -cp "lib/*" \
+     -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" \
+     org.sensorhub.impl.SensorHub \
+     config.json \
+     db
